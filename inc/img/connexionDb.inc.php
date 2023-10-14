ec138de8774818d1261eeb5cece7833becfca5c1
Bastien Duliege <bastienduliege@outlook.fr>
2023-10-14 12:31:09 +0200

    ConnexionDB revu pour webhost ne fonctionne pas

--- inc/img/connexionDb.inc.php
+++ inc/img/connexionDb.inc.php
@@ -1,9 +1,19 @@
 <?php
 
+// Si je me connecte en local
+
 $username = "root";
 $password = '';
 $dsn = 'mysql:host=localhost;dbname=dbbootic;port=3306;charset=utf8';
 $maBase = new PDO($dsn, $username, $password);
 $maBase->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+// si je me connecte dans webHost
+
+// $username = "id21170761_dbbootic";
+// $password = 'Dbbootic1!';
+// $dsn = 'mysql:host=localhost;dbname=dbbootic;port=3306;charset=utf8';
+// $maBase = new PDO($dsn, $username, $password);
+// $maBase->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 ?>
